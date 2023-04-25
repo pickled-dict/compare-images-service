6448c5fc4c2a92d1cd7967e72f0653697711023f
pickled-dict <mathew_forgione@proton.me>
2023-04-25 17:07:12 -0300

    added a little bit more oomf to the script
    - added .env so its a bit easier to reuse the script (python-dotenv)
    - added docker client package to check the state of containers/images
    - made the `DOCKER_RUN_CMD` string able to receive the new environment
      variables
    - minor cleanup (log and template literals)
    - automatic container starts and stops depending on if container/images
      are available, deleted, etc.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Docker Image Compare
+
+A simple script that checks if container is stopped/exist, and if image exists and is updated
+
+## Install
+
+Not going to bother going all out with virtualenvs/pipenv here -- just install the `docker` and `python-dotenv` packages:
+
+`pip install docker python-dotenv`
+
+## Environment Variables
+
+See example `.env` file (`.env.example`)
--- compare-images.py
+++ compare-images.py
@@ -2,29 +2,83 @@ import os
 import sys
 import json
 import time
+import docker
+from dotenv import dotenv_values
 
-DOCKER_RUN_CMD = """docker run -p 3000:3000 --name <REPLACE ME> \\
--e DATABASE_URL="<REPLACE ME>" \\
--e NEXTAUTH_URL="<REPLACE ME>" \\
--e GITHUB_CLIENT_SECRET="<REPLACE ME>" \\
--e NEXTAUTH_SECRET="<REPLACE ME>" \\
--d <REPLACE ME>"""
 
+env = dotenv_values('.env')
+docker_client = docker.from_env()
+
+CONTAINER_NAME          = env['CONTAINER_NAME']
+DATABASE_URL            = env['DATABASE_URL'] 
+NEXTAUTH_URL            = env['NEXTAUTH_URL']
+GITHUB_CLIENT_ID        = env['GITHUB_CLIENT_ID']
+GITHUB_CLIENT_SECRET    = env['GITHUB_CLIENT_SECRET']
+NEXTAUTH_SECRET         = env['NEXTAUTH_SECRET']
+IMAGE_NAME              = env['IMAGE_NAME'] 
+
+DOCKER_RUN_CMD = """docker run -p 3000:3000 --name {0} \\
+-e DATABASE_URL="{1}" \\
+-e NEXTAUTH_URL="{2}" \\
+-e GITHUB_CLIENT_ID="{3}" \\
+-e GITHUB_CLIENT_SECRET="{4}" \\
+-e NEXTAUTH_SECRET="{5}" \\
+-d {6}
+""".format(
+    CONTAINER_NAME,
+    DATABASE_URL,
+    NEXTAUTH_URL,
+    GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET,
+    NEXTAUTH_SECRET,
+    IMAGE_NAME
+)
+
+
+def log(s: str):
+    print(s)
+    sys.stdout.flush()
+
+
+# check docker client to see if image with image name exists
+try:
+    docker_client.images.get(IMAGE_NAME)
+    log(f'Image with name {IMAGE_NAME} exists')
+except:
+    log(f'Image with name {IMAGE_NAME} was not found')
+    os.system(f'docker pull {IMAGE_NAME}')
+    log('Image pulled from Dockerhub')
+
+# check docker client to see if container exists -- if not, 
+# start, if exists but is stopped, start stopped container
+try:
+    container = docker_client.containers.get(CONTAINER_NAME)
+    if container.attrs['State']['Running'] == False:
+        log(f'Container with name {CONTAINER_NAME} is not currently running')
+        os.system(f'docker container start {CONTAINER_NAME}')
+        log('Stopped container has been restarted')
+except:
+    log('Docker container not found, starting a fresh container')
+    os.system(DOCKER_RUN_CMD)
+    log('Fresh container started')
+
+
+# main loop that checks if the current image matches the remote one
 while True:
-    old = json.loads(os.popen('docker image inspect <REPLACE ME>').read())[0]['Id']
-    os.system('docker pull <REPLACE ME>')
-    new = json.loads(os.popen('docker image inspect <REPLACE ME>').read())[0]['Id']
+    old = json.loads(os.popen(f'docker image inspect {IMAGE_NAME}').read())[0]['Id']
+    os.system(f'docker pull {IMAGE_NAME}')
+    new = json.loads(os.popen(f'docker image inspect {IMAGE_NAME}').read())[0]['Id']
 
     equal = old == new
 
     print(old)
     print(new)
-    print(f"are they equal?: {equal}")
+    print(f"equal?: {equal}")
     sys.stdout.flush()
 
     if not equal:
-        os.system('docker container stop <REPLACE ME>')
-        os.system('docker container rm <REPLACE ME>')
+        os.system(f'docker container stop {CONTAINER_NAME}')
+        os.system(f'docker container rm {CONTAINER_NAME}')
         os.system(DOCKER_RUN_CMD)
 
     time.sleep(10)
